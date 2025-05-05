c3056fb6565c6085f534581cfec5d238ea5a8f7b
Vinicius <106980142+vinih3m@users.noreply.github.com>
2025-05-05 14:45:40 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,15 +8,11 @@ public class Main {
 
         l[1] = new Lutador("Vinícius", "Brasil", 23, 1.85f, 57.8f, 14, 2, 1);
 
-        l[2] = new Lutador("Vitor", "Argentina", 25, 1.65f, 80.9f, 13, 2, 1);
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[1], l[1]);
+        UEC01.lutar();
 
-        l[3] = new Lutador("Walter", "Holanda", 45, 1.68f, 81.6f, 12, 2, 1);
 
-        l[4] = new Lutador("Thomas", "EUA", 18, 1.54f, 119.3f, 15, 2, 1);
-
-        l[5] = new Lutador("Mateo", "Espanha", 22, 1.77f, 105.7f, 14, 0, 1);
-
-        l[4/3].apresentar();
 
     }
 }
